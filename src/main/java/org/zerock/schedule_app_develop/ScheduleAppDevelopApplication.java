17a389038f12241b60994ccca2c8c73c49e2cca9
Seungyoung <leesy960909@gmail.com>
2026-01-09 11:39:38 +0900

    bug : JPA Auditing 세팅 버그 수정

--- src/main/java/org/zerock/schedule_app_develop/ScheduleAppDevelopApplication.java
+++ src/main/java/org/zerock/schedule_app_develop/ScheduleAppDevelopApplication.java
@@ -2,10 +2,11 @@ package org.zerock.schedule_app_develop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaAuditing
 public class ScheduleAppDevelopApplication {
 
     public static void main(String[] args) {
