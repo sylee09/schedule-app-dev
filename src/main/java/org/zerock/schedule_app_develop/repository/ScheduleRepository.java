067f37cf442b33a6da9ebd9b8fc70c38c80e3f2e
Seungyoung <leesy960909@gmail.com>
2026-01-09 22:07:30 +0900

    fix : N+1 문제 해결
    Schedule의 ResponseDto가 User의 이름을 반환한다.
    따라서 ResponseDto를 만들때마다 실제 User의 데이터를 찾는 쿼리가 추가적으로 발생. 이를 left join fetch로 해결

--- src/main/java/org/zerock/schedule_app_develop/repository/ScheduleRepository.java
+++ src/main/java/org/zerock/schedule_app_develop/repository/ScheduleRepository.java
@@ -1,7 +1,16 @@
 package org.zerock.schedule_app_develop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.schedule_app_develop.entity.Schedule;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    @Query("select s from Schedule s left join fetch s.user where s.id=:id")
+    Optional<Schedule> findById(Long id);
+
+    @Query("select s from Schedule s left join fetch s.user")
+    List<Schedule> findAll();
 }
