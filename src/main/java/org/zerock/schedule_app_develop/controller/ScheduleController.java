4b94a6c836836c14cca25fcf21a58a5ab50b3a3c
Seungyoung <leesy960909@gmail.com>
2026-01-09 17:27:40 +0900

    feat : controller에서 전체 조회 구현 및 테스트 수행

--- src/main/java/org/zerock/schedule_app_develop/controller/ScheduleController.java
+++ src/main/java/org/zerock/schedule_app_develop/controller/ScheduleController.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.schedule_app_develop.dto.ScheduleCreateRequestDto;
 import org.zerock.schedule_app_develop.dto.ScheduleResponseDto;
 import org.zerock.schedule_app_develop.service.ScheduleService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +23,11 @@ public class ScheduleController {
     @PostMapping
     public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleCreateRequestDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.createSchedule(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScheduleResponseDto>> getAllSchedules() {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.viewAllSchedules());
     }
 
 }
--- src/test/java/org/zerock/schedule_app_develop/controller/ScheduleControllerTest.java
+++ src/test/java/org/zerock/schedule_app_develop/controller/ScheduleControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.zerock.schedule_app_develop.dto.ScheduleCreateRequestDto;
 import org.zerock.schedule_app_develop.dto.ScheduleResponseDto;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -29,5 +31,18 @@ class ScheduleControllerTest {
         assertThat(schedule.getBody().getContent()).isEqualTo(dto.getContent());
         assertThat(schedule.getBody().getSubject()).isEqualTo(dto.getSubject());
         assertThat(schedule.getBody().getModifiedTime()).isEqualTo(schedule.getBody().getCreatedTime());
+    }
+
+    @Test
+    @DisplayName("getAllSchedules")
+    void getAllSchedules() {
+        ScheduleCreateRequestDto dto1 = new ScheduleCreateRequestDto("lee", "제목", "내용");
+        scheduleController.createSchedule(dto1);
+        ScheduleCreateRequestDto dto2 = new ScheduleCreateRequestDto("lee", "제목", "내용");
+        scheduleController.createSchedule(dto2);
+
+        ResponseEntity<List<ScheduleResponseDto>> allSchedules = scheduleController.getAllSchedules();
+        assertThat(allSchedules.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(allSchedules.getBody().size()).isEqualTo(2);
     }
 }
